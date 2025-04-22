705c102bcbd26ae83c6c197e15e61333a9438352
Seungjun(June) Kang <sjkangsd@gmail.com>
2025-04-22 17:16:42 -0400

    added docstrings to Budget_Calendar class

--- classes.py
+++ classes.py
@@ -1,12 +1,23 @@
 import calendar as cl
 import datetime as dt
 import math
+import decimal
 
 def truncate_to_hundredths(number):
   return math.floor(number * 100) / 100
 
 class Budget_Calendar:
+    """Class for a Budget Calendar Object. A budget calendar is a dictionary with date objects as keys and list of Expenditure object as values.
+
+    Attributes:
+        calendar (dictionary): Dictionary with date objects as keys and list of Expenditure object as values
+    """
     def __init__(self, year):
+        """Initializes a Budget_Calendar object.
+
+        Args:
+            year (int): The year the calendar will be on
+        """
         cal = cl.Calendar()
         yearly_calendar = {}
         for month in range(1, 13):
@@ -17,17 +28,43 @@ class Budget_Calendar:
         self.calendar = yearly_calendar
     
     def add_expenditure(self, date, spending):
-        self.yearly_calendar[date].append(spending)
+        """Adds an Expenditure object to the corresponding date
+
+        Args:
+            date (Date object): Date the expense was made
+            spending (Expenditure object): The expenditure that was made
+        
+        Side Effect:
+            Adds the Expenditure object into the list which is the value for corresponding Date key in the self.calendar dictionary
+        """
+        self.calendar[date].append(spending)
     
     def get_daily_spending_amount(self, date):
+        """Calculates total amount of money spent on particular date
+
+        Args:
+            date (Date object): Date to get the total amount of money spent for
+        
+        Returns:
+            total_spent (Decimal object): The total amount of money spent on the passed in date
+        """
         total_spent = 0
 
-        for spending in self.yearly_calendar[date]:
+        for spending in self.calendar[date]:
             total_spent += spending.amount
         
         return total_spent
     
     def get_total_spending_amount_between_two_dates(self, starting_date, end_date):
+        """Calculates total amount of money spend between two particular dates
+
+        Args:
+            starting_date (Date object): Starting date of the range in which to get the total spending for
+            end_date (Date object): Ending date of the range in which to get the total spending for
+
+        Returns:
+            total_spent (Decimal object): The total amount of money spent during the two particular dates that were passed in
+        """
         current_date = starting_date
         total_spent = 0
         while current_date <= end_date:
@@ -109,12 +146,11 @@ class Expenditure:
         category (String): What category the expenditure is for. i.e. Leisure, food, hobby, etc.
         date (String): Day it was made in the format of month/day/year
     """
-    def __init__(self, description, amount, type, category, date):
+    def __init__(self, description, amount, type, category):
         self.description = description
-        self.amount = amount
+        self.amount = decimal.Decimal(str(amount))
         self.type = type
         self.category = category
-        self.date = date
     
     def __repr__(self):
         return f"Date: {self.date}\n{self.description}\nAmount: {self.amount}$\nType: {self.type}\nCategory: {self.category}"
@@ -124,15 +160,25 @@ def calculate_daily_budget(fund, start_date, end_date):
     
     return fund / days_between
 
-    
-myBudget_recommendation = Budget_recommendation(1200, "monthly", 2, 2025)
-print(myBudget_recommendation)
+if __name__ == "__main__":
+    """
+    myBudget_recommendation = Budget_recommendation(1200, "monthly", 2, 2025)
+    print(myBudget_recommendation)
 
-myExpenditure1 = Expenditure("Spotify", 9.99, "Fixed", "Music", dt.date(2025, 4, 22))
-myExpenditure2 = Expenditure("Spotify", 9.99, "Fixed", "Music", dt.date(2025, 4, 23))
-print(myExpenditure1)
-print([myExpenditure1, myExpenditure2])
+    myExpenditure1 = Expenditure("Spotify", 9.99, "Fixed", "Music", dt.date(2025, 4, 22))
+    myExpenditure2 = Expenditure("Spotify", 9.99, "Fixed", "Music", dt.date(2025, 4, 23))
+    print(myExpenditure1)
+    print([myExpenditure1, myExpenditure2])
 
 
-myBudget = calculate_daily_budget(1200, dt.date(2025, 4, 1), dt.date(2025, 4, 30))
-print(myBudget)
+    myBudget = calculate_daily_budget(1200, dt.date(2025, 4, 1), dt.date(2025, 4, 30))
+    print(myBudget)
+    """
+
+    myBudget_Calendar = Budget_Calendar(2025)
+    myBudget_Calendar.add_expenditure(dt.date(2025,4,22), Expenditure("Spotify", 9.99, "Fixed", "Music"))
+    myBudget_Calendar.add_expenditure(dt.date(2025,4,22), Expenditure("Spotify", 20.99, "Fixed", "Music"))
+    myBudget_Calendar.add_expenditure(dt.date(2025,4,23), Expenditure("Spotify", 30.99, "Fixed", "Music"))
+    print(myBudget_Calendar.get_daily_spending_amount(dt.date(2025,4,22)))
+    print(myBudget_Calendar.get_daily_spending_amount(dt.date(2025,4,23)))
+    print(myBudget_Calendar.get_total_spending_amount_between_two_dates(dt.date(2025,4,22), dt.date(2025,4,23)))
