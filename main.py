d4e08886d04894e79d923393b13317824ac0884c
Seungjun(June) Kang <sjkangsd@gmail.com>
2025-05-05 13:58:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,26 +22,36 @@ class User_Interface:
         self.main_menu()
 
     def print_line(self):
+        """A method that prints out a dash line
+
+        Side Effect:
+            Prints out a line with 60 dashes
+        """
         print("------------------------------------------------------------")        
     
     def main_menu(self):
+        """A method that prompts the main menu to the user
+
+        Side Effect:
+            Prompts the user between adding income or expense, or run a calculation on the Calendar.
+        """
         wrong_input = True
 
         while wrong_input:
             self.print_line()
-            print("What would you like to do?\n - Add Income\n - Add Expense\n - Calculate")
+            print("What would you like to do?\n - Add Income\n - Add Expense\n - Calculate Funds")
             self.print_line()
             user_input = input()
-            if user_input.casefold() == "Add Income".casefold():
+            if user_input.casefold() == "Add Income".casefold(): # If the user chooses to add income to the calendar
                 self.add_income_prompt()
                 wrong_input = False
-            elif user_input.casefold() == "Add Expense".casefold():
+            elif user_input.casefold() == "Add Expense".casefold(): # If the user chooses to add expense to the calendar
                 self.add_expense_prompt()
                 wrong_input = False
-            elif user_input.casefold() == "Calculate".casefold():
+            elif user_input.casefold() == "Calculate Funds".casefold(): # If the user chooses to calculate funds on the calendar
                 self.calculate_prompt()
                 wrong_input = False
-            else:
+            else: # If the user input does not match any of the options
                 print("Please enter correctly.")
 
     
