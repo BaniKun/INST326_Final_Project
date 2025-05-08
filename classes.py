2f435192458d8f6506fdec83dbc28ea25a3a8785
Seungjun(June) Kang <sjkangsd@gmail.com>
2025-05-07 21:38:06 -0400

    added a method that calculates recommended budget

--- classes.py
+++ classes.py
@@ -22,6 +22,7 @@ class Budget_Calendar:
         cal = cl.Calendar()
         yearly_expense_calendar = {}
         yearly_income_calendar = {}
+        self.year = year
         for month in range(1, 13):
             for day in cal.itermonthdates(year, month):
                 if day.year == year:
@@ -188,6 +189,13 @@ class Budget_Calendar:
         total = self.get_total_income_amount_between_two_dates(starting_date, end_date) - self.get_total_spending_amount_between_two_dates(starting_date, end_date)
         
         return total
+    
+    def get_recommended_daily_budget(self, starting_date, end_date):
+        total_fund = self.get_total_fund_left_between_two_dates(dt.date(self.year,1,1), end_date)
+        num_of_days = abs((end_date - starting_date).days) + 1
+        
+        return truncate_to_hundredths(total_fund / num_of_days)
+
 
 # Test functions for saveing and loading
 def save_calendar(calendar):
@@ -264,7 +272,7 @@ if __name__ == "__main__":
     myBudget_Calendar.add_income(dt.date(2025,4,23), Income("Pocket Money", 10.0, "Not Fixed"))
     print(myBudget_Calendar.get_daily_income_amount(dt.date(2025,4,22)))
     print(myBudget_Calendar.get_daily_income_amount(dt.date(2025,4,23)))
-    print(myBudget_Calendar.get_total_income_amount_between_two_dates(dt.date(2025,4,22), dt.date(2025,4,23)))
+    print(myBudget_Calendar.get_total_income_amount_between_two_dates(dt.date(2025,4,22), dt.date(2025,4,22)))
     myBudget_Calendar.add_fixed_income(dt.date(2025,4,25), Income("Pocket Money", 10.0, "Fixed"), "monthly")
     print(myBudget_Calendar.get_daily_income_amount(dt.date(2025,4,25)))
     print(myBudget_Calendar.get_daily_income_amount(dt.date(2025,5,25)))
@@ -275,8 +283,16 @@ if __name__ == "__main__":
     print(myBudget_Calendar.get_daily_change_in_fund(dt.date(2025,4,23)))
     print(myBudget_Calendar.get_total_fund_left_between_two_dates(dt.date(2025,4,22), dt.date(2025,4,23)))
 
+    print("calculate test")
+    print(myBudget_Calendar.get_total_spending_amount_between_two_dates(dt.date(2025,4,22), dt.date(2025,5,25)))
+    print(myBudget_Calendar.get_total_income_amount_between_two_dates(dt.date(2025,4,22), dt.date(2025,5,25)))
+    print(abs((dt.date(2025,5,25) - dt.date(2025,4,22)).days) + 1)
+    print(myBudget_Calendar.get_recommended_daily_budget(dt.date(2025,4,22), dt.date(2025,5,25)))
+
+    """
     print("save test")
     save_calendar(myBudget_Calendar)
     newCalendar = load_calendar()
     print(newCalendar.get_daily_income_amount(dt.date(2025,4,22)))
     print(newCalendar.get_total_fund_left_between_two_dates(dt.date(2025,4,22), dt.date(2025,4,23)))
+    """
